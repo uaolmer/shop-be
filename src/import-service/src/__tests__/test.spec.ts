c0367f018ce315be990f28783abb54d39627ec2b
Daniil <uaolmer2@gmail.com>
2021-09-26 23:25:47 +0300

    fix: restore tests file

--- src/import-service/src/__tests__/test.spec.ts
+++ src/import-service/src/__tests__/test.spec.ts
@@ -2,6 +2,7 @@ import { main as importProductsFile } from "../functions/import-products-file/ha
 import * as S3 from 'aws-sdk/clients/s3';
 import { IMock } from '../interfaces/interface';
 
+import { BUCKET } from '../constants/constants';
 
 jest.mock('aws-sdk');
 
@@ -19,6 +20,21 @@ describe('service importProductsFile()', (): void => {
     it('should initialize S3 instance', async (): Promise<void> => {
       await importProductsFile(mock, undefined, undefined);
       expect(S3).toHaveBeenCalledTimes(1);
+    });
+
+    it('should properly call \'getSignedUrlPromise\' method of S3 object', async (): Promise<void> => {
+      await importProductsFile(mock, undefined, undefined);
+
+      const response = S3['mock'].instances[0].getSignedUrlPromise;
+
+      expect(response).toHaveBeenCalledWith('putObject', {
+        Bucket: BUCKET,
+        Key: 'uploaded/mockedName',
+        ContentType: 'text/csv',
+        Expires: 60,
+      });
+
+      expect(response).toHaveBeenCalledTimes(1);
     });
   
 });
